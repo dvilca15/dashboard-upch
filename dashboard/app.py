ed6a478c597d31ba7aae1348a0c09515af827977
DANILO ANDRE VILCA MACEDO <70995003@upch.edu.pe>
2025-08-25 17:26:28 -0500

    Fix: expose server for Render

--- dashboard/app.py
+++ dashboard/app.py
@@ -107,3 +107,6 @@ if __name__ == "__main__":
     app = main()
     port = int(os.environ.get("PORT", 8050))  # Render asigna el puerto
     app.run(host="0.0.0.0", port=port, debug=False)
+
+app = main()
+server = app.server
